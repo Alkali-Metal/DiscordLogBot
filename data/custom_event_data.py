d2c3cfd8d23d950e6e348c4de23ba75a203e3c0b
Alkali-Metal <Alkali.maps@gmail.com>
2017-10-02 16:37:36 -0600

    [Custom Data] Begin mapping custom data variables to a specific file for multi-file reference.

---
+++ data/custom_event_data.py
@@ -0,0 +1,26 @@
+custom_data = {
+    "USER_JOIN":[
+        "response":[
+            "rich",
+            "fanceh",
+            "compact"
+        ],
+        "title":[
+            "rich",
+            "fanceh"
+        ],
+        "content":[
+            "rich"
+        ]
+    ],
+    "MESSAGE_EDIT":[
+        "ignored_channels",
+        "ignored_users"
+    ],
+    "MESSAGE_DELETE":[
+        "ignored_channels",
+        "ignored_users"
+    ]
+}
+
+
