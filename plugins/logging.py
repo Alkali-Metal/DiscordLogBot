aa7e5c9da1f92b6f1d4487bd1571ef4036bea99f
Alkali-Metal <Alkali.maps@gmail.com>
2017-10-08 22:12:05 -0600

    Do some stuff?

--- plugins/logging.py
+++ plugins/logging.py
@@ -1,8 +1,10 @@
 from data.storage import load
 from data.storage import delete_guild
+#from data.storage import create_guild
 from data.functions import load_config
 from data.functions import create_log_embed
 from data.functions import response_to_class
+#from data.functions import create_custom_message
 from data.custom_event_data import custom_data
 
 from holster.emitter import Priority
@@ -47,6 +49,43 @@ class LoggingPlugin(Plugin):
     #TODO: Emojis Updated
     #TODO: Integrations Updated
     #TODO: Member Joined (And random string variant)
+    @Plugin.listen("GuildMemberAdd")
+    def member_join_static(self, event):
+        log_event = "USER_JOIN"
+        guilds = load()
+        guild_id = str(event.member.guild_id)
+        user = event.member
+        send_message = self.bot.client.api.client.channels_messages_create
+        
+        
+        if guild_id not in guilds:
+            return
+        
+        
+        if len(guilds[guild_id]) == 0:
+            return
+        
+        
+        for log in guilds[guild_id]:
+            if log_event in log["events"]:
+                if log_event in log["data"]:
+                    return
+                    #TODO: Custom data handling as per custom processing
+                    #create_custom_message(log)
+            else:
+                #TODO: Default event handling
+                if log["type"] == "rich":
+                    response = create_log_embed(log_event, event)
+                    
+                    send_message(int(log),
+                                 content=response[0],
+                                 embed=response[1])
+                else:
+                    send_message(int(log),
+                                 response_to_class(log["type"], log_event))
+
+
+
     #TODO: Member Leave
     #TODO: Member Updated
     #TODO: Role Created
